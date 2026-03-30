681543e54e51d1713c407091d19c9c22134d494f
anishmorde <anishmorde@gmail.com>
2026-03-30 10:25:40 +0000

    hello.c changes

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    printf("Welcome to C programming.\n");
+    printf("This is a second message.\n");
+    printf("Printing multiple messages is easy!\n");
+    return 0;
+}
